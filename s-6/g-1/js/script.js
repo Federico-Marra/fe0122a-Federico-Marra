602133eddcea7d31e849666fa7c0cda965cb1292
Federico Marra <fede@MacBook-Air-di-Federico.local>
2022-03-02 15:05:55 +0100

    esercizio s6/g3

--- s-6/g-1/js/script.js
+++ s-6/g-1/js/script.js
@@ -7,47 +7,55 @@ var pulisci = $('#pulisci-form');
 
 $(document).ready(function () {
     $(iscriviti).click(function () {
+        nome = $('#nome').val();
         email1 = $('#email-1').val();
         email2 = $('#email-2').val();
-        let invia = verificatore();
+        let isValid = true;
+        //let campi = $('#form-email input[type='text']');
+        /*createImageBitmap.each(function(indice, elemento){
+            if(elemento.val() == ''){
+                $(elemento).next().text('* Richiesto!');
+                isValid = false;
+            }else{
+                $(elemento).next().text();
+            }
+        });*/
 
-        if (nome.val() == '' || nome.val() === '') {
-
+        if (nome == '' || nome === '') {
             $('#nome').css('border', '2px solid red');
             $('#nome').next('span').text('* richiesto!').css({
                 'color': 'red',
                 'display': 'inline'
             }).fadeOut(2000);
             console.log('i dati inseriti non sono corretti!');
+            isValid = false;
 
-        } else if (!(nome.val() == '') || !(nome.val() === '')) {
-
+        } else if (!(nome == '') || !(nome === '')) {
             $('#nome').css('border', '2px solid green');
             console.log('ok');
-
         }
         if (email1 == '' || email1 == 'undefined') {
-
             $('#email-1').css('border', '2px solid red');
             $('#email-1').next('span').text('* richiesto!').css({
-                'color':'red',
-                'display':'inline'
+                'color': 'red',
+                'display': 'inline'
             }).fadeOut(2000);
             console.log('i dati inseriti non sono corretti!');
+            isValid = false;
 
-        } else if (!(email1 == '')|| !(email1 == 'undefined')) {
+        } else if (!(email1 == '') || !(email1 == 'undefined')) {
 
             $('#email-1').css('border', '2px solid green');
             console.log('ok');
 
         }
         if (!(email2 == email1) || email2 == '') {
-
             $('#email-2').css('border', '2px solid red');
             $('#email-2').next('span').text('* richiesto!').css({
-                'color':'red',
-                'display':'inline'
+                'color': 'red',
+                'display': 'inline'
             }).fadeOut(2000);
+            isValid = false;
             console.log('i dati non combaciano!');
 
         } else if (email2 == email1 && !(email2 == '')) {
@@ -56,10 +64,15 @@ $(document).ready(function () {
             console.log('ok');
 
         }
-        function verificatore(){
-            if (email2 == email1 && email2 === email1 && !(email2 == '') && !(email1 == '')){
+
+        if (isValid) {
+            $('#form-email').submit()
+        }
+
+        function verificatore() {
+            if (email2 == email1 && email2 === email1 && !(email2 == '') && !(email1 == '')) {
                 alert('Ti sei iscritto !!!');
-            }else if (!(email2 == email1) && !(email2 === email1) || email1 == ''){
+            } else if (!(email2 == email1) && !(email2 === email1) || email1 == '') {
                 alert('Ops! Qualcosa è andato storto.. Contolla nuovamente i tuoi dati!');
             }
         }
@@ -77,12 +90,11 @@ $(document).ready(function () {
     });
 });
 
-
 $(document).ready(function () {
-    let input = $('#form-email').next('input').val();
+    let input = $('#form-email input[type="text"]').val();
     $(pulisci).click(function () {
-       if (input == 'text' || input == Number){
-           $(input).remove();
-       } 
+        if (input == String || input == Number) {
+            $(input).remove();
+        }
     });
 });
---
+++ s-6/g-2/script.js
@@ -0,0 +1,12 @@
+// Quando il documento è pronto...
+$( () => {
+        $('h2').click(function(){
+            $(this).toggleClass('meno').next().toggle('menu-tendina');
+        });
+})
+    // collega i gestori evento per il click per tutti i tag h2.
+        // crea una variabile ed immagazzina al suo interno il tag h2 che inizializza l'evento (usa .currentTarget).        
+        // attiva la classe .meno dei CSS sul tag h2.
+        // mostra o nascondi il div, utilizza un if-else.     
+        // utilizza il prepreventDefault() per prevenire che il gestore eventi di default sia eseguito.
+    // imposta il focus sul primo tag ancor dell'h2
