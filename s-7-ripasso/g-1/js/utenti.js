d097e6feb1254e2c87fcad099c6bb993d9de4d22
Federico Marra <fede@MacBook-Air-di-Federico.local>
2022-03-12 18:33:24 +0100

    esercizi settimana di ripasso

--- s-7-ripasso/g-1/js/utenti.js
+++ s-7-ripasso/g-1/js/utenti.js
@@ -5,28 +5,39 @@ function stampaUtente() {
     var dataBase = localStorage.getItem('utentiSalvati');
     var arrayUtenti = JSON.parse(dataBase);
 
+
     arrayUtenti.forEach(function (element) {
         var tr = document.createElement('tr')
-        posizione.prepend(tr);
-        var trCreata = document.querySelector("tr")
-
+        posizione.append(tr);
 
         for (utentiSalvati in element) {
             var td = document.createElement('td')
-            trCreata.prepend(td)
-            document.querySelector("td").innerHTML = element[utentiSalvati]
+            tr.append(td)
+            td.innerHTML = element[utentiSalvati]
         }
         var cancella = document.createElement("button")
         cancella.classList.add('cancellaUtente')
         cancella.innerHTML = 'elimina'
-        trCreata.append(cancella)
+
+        tr.append(cancella)
+        
         cancella.addEventListener('click', function () {
             tr.remove();
             eliminaUtente();
-
         })
 
-    })
+        var mostra = document.createElement('button')
+        mostra.classList.add('mostraBtn')
+        mostra.innerHTML = 'mostra'
+
+        tr.append(mostra)
+
+        mostra.addEventListener('click', function(){            
+            let trpassword = document.querySelectorAll('tr:nth-child(3)'); 
+            console.log(trpassword.value)
+            
+        })
+})
 
     function eliminaUtente(user) {
         let ls = localStorage.getItem('utentiSalvati')
@@ -37,5 +48,15 @@ function stampaUtente() {
         var indexElement = db.indexOf(indiceElemento)
         db.splice(indexElement, 1)
         localStorage.setItem('utentiSalvati', JSON.stringify(db))
+    }
+    function nascondi(){
+        let tdE = localStorage.getItem('utentiSalvati')
+        let data = JSON.parse(tdE);
+        let eliminaTd = data.find(function (ele){
+            return utentiSalvati.password
+        })
+        var indexTd = data.indexOf(eliminaTd)
+        //data.splice(indexTd, 1)
+        localStorage.setItem('utentiSalvati', JSON.stringify(data))
     }
 }
